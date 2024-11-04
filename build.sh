0be9bfe504ad03ba4ed3c5d86653679c9a579930
rik-x777 <aritrarik536@protonmail.com>
2024-11-04 21:59:56 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,18 @@
-
+rm -rf .repo/local_manifests; \
+rm -rf device/xiaomi/munch; \
+rm -rf device/xiaomi/sm8250-common; \
+rm -rf kernel/xiaomi/sm8250; \
+rm -rf vendor/xiaomi/munch; \
+rm -rf vendor/xiaomi/sm8250-common; \
+rm -rf hardware/xiaomi; \
+rm -rf vendor/lineage-priv; \
+repo init --depth=1 -u https://github.com/AfterlifeOS/android_manifest.git -b 14 --git-lfs; \
+/opt/crave/resync.sh; \
+git clone https://gitea.com/rik-x290/device.git -b afterlife device/xiaomi/munch; \
+git clone https://gitea.com/rik-x290/common.git -b afterlife device/xiaomi/sm8250-common; \
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi; \
+git clone https://github.com/rik-x777/vendor.git vendor/xiaomi/sm8250-common; \
+git clone https://github.com/AnkitSavani111/android_kernel_xiaomi_sm8250.git kernel/xiaomi/sm8250; \
+git clone https://github.com/TheMuppets/proprietary_vendor_xiaomi_munch.git vendor/xiaomi/munch; \
+. build/envsetup.sh; \
+goafterlife munch
