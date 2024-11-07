3e2ab30d30a0109a6ebc02ac6a2e58ae0597ced6
rik-x777 <aritrarik536@protonmail.com>
2024-11-07 16:05:35 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,11 +8,11 @@ rm -rf vendor/xiaomi/munch; \
 rm -rf vendor/xiaomi/sm8250-common; \
 rm -rf hardware/xiaomi; \
 rm -rf vendor/lineage-priv; \
-git clone https://github.com/rik-x777/device_afterlife.git device/xiaomi/munch; \
-git clone https://github.com/rik-x777/common_afterlife.git device/xiaomi/sm8250-common; \
+git clone https://github.com/rik-x777/da.git device/xiaomi/munch; \
+git clone https://github.com/rik-x777/ca.git device/xiaomi/sm8250-common; \
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi; \
-git clone https://github.com/rik-x777/vendor.git -b afterlife vendor/xiaomi/sm8250-common; \
+git clone https://gitea.com/deadlyshroud/vendor_xiaomi_sm8250-common.git vendor/xiaomi/sm8250-common; \
 git clone https://github.com/AnkitSavani111/android_kernel_xiaomi_sm8250.git kernel/xiaomi/sm8250; \
-git clone https://github.com/rik-x777/proprietary_vendor_xiaomi_munch.git vendor/xiaomi/munch; \
+git clone https://gitea.com/deadlyshroud/vendor_xiaomi_munch.git vendor/xiaomi/munch; \
 . build/envsetup.sh; \
 goafterlife munch
