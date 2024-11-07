f2d7ba21eecdb7a9eefc98ab1cf81c9f0527c284
rik-x777 <aritrarik536@protonmail.com>
2024-11-08 01:35:33 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,8 +11,8 @@ rm -rf vendor/lineage-priv; \
 git clone https://codeberg.org/rik/android_device_xiaomi_munch.git -b infinity device/xiaomi/munch; \
 git clone https://codeberg.org/rik/android_device_xiaomi_sm8250-common.git -b infinity device/xiaomi/sm8250-common; \
 git clone https://github.com/LineageOS/android_kernel_xiaomi_sm8250.git kernel/xiaomi/sm8250; \
-git clone https://codeberg.org/rik/proprietary_vendor_xiaomi_munch.git -b aosp vendor/xiaomi/munch; \
-git clone https://codeberg.org/rik/proprietary_vendor_xiaomi_sm8250-common.git -b aosp vendor/xiaomi/sm8250-common; \
+git clone https://github.com/rik-x777/proprietary_vendor_xiaomi_munch.git -b aosp vendor/xiaomi/munch; \
+git clone https://github.com/rik-x777/proprietary_vendor_xiaomi_sm8250-common.git -b aosp vendor/xiaomi/sm8250-common; \
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi; \
 . build/envsetup.sh; \
 lunch infinity_munch-user; \
