3f5f6df485f9b65320482e143104b5dc7e415c72
Rik <aritrarik536@protonmail.com>
2024-12-07 22:21:12 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -14,8 +14,8 @@ rm -rf vendor/xiaomi/miuicamera; \
 git clone https://github.com/rik-x777/android_device_xiaomi_munch.git device/xiaomi/munch; \
 git clone https://github.com/rik-x777/android_device_xiaomi_sm8250-common.git device/xiaomi/sm8250-common; \
 git clone https://github.com/EmanuelCN/kernel_xiaomi_sm8250.git -b munch kernel/xiaomi/sm8250; \
-git clone https://github.com/TheMuppets/proprietary_vendor_xiaomi_munch.git vendor/xiaomi/munch; \
-git clone https://github.com/TheMuppets/proprietary_vendor_xiaomi_sm8250-common.git vendor/xiaomi/sm8250-common; \
+git clone https://github.com/rik-x777/proprietary_vendor_xiaomi_munch.git vendor/xiaomi/munch; \
+git clone https://github.com/rik-x777/proprietary_vendor_xiaomi_sm8250-common.git vendor/xiaomi/sm8250-common; \
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-22.0 hardware/xiaomi; \
 git clone https://gitea.com/hdzungx/android_vendor_xiaomi_miuicamera.git vendor/xiaomi/miuicamera; \
 git clone https://gitea.com/hdzungx/android_vendor_xiaomi_munch-firmware.git vendor/xiaomi/munch-firmware; \
